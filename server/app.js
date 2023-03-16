3cbe4079edc4eed93c1e1f0a0a57395ab1bda8e4
hugo-baptista <pg50416@alunos.uminho.pt>
2023-03-16 20:30:42 +0000

    Update Co-authored-by: Tomás Lima <Tomaicho@users.noreply.github.com> Co-authored-by: GoncaloCar <GoncaloCar@users.noreply.github.com>

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var loginRouter = require('./routes/login');
 
 var app = express();
 
@@ -13,5 +13,5 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/login', loginRouter);
 module.exports = app;
