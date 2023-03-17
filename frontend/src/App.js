86984425f12c82a23bfd15d0ff0933f9e4c89656
hugo-baptista <pg50416@alunos.uminho.pt>
2023-03-17 14:44:44 +0000

    Undo changes App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route exact path="/">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </Route>
-        <Route path="/login" render={() => <iframe src="./components/login.html" />} />
-      </Switch>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
 export default App;
+
