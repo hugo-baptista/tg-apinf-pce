628a254a02d623a12762e8f5735cd214e73bf621
hugo-baptista <pg50416@alunos.uminho.pt>
2023-05-06 14:11:11 +0100

    Update

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,18 +1,9 @@
 import { useContext } from "react";
 import { UserContext } from "./static/UserContext";
-import useFetch from "./useFetch";
+import AssessmentIcon from '@mui/icons-material/Assessment';
+import { IconContext } from "react-icons";
 
 const Home = () => {
-    // const {data: blogs, isLoading, error} = useFetch("http://localhost:8000/blogs");
-
-    // return (
-    //     <div className="home">
-    //         {error && <div>{error}</div>}
-    //         {isLoading && <div id="loading">Loading...</div>}
-    //         {blogs && <BlogList blogs={blogs} title="All Blogs:" />}
-    //     </div>
-    // );
-
     const {user, setUser} = useContext(UserContext);
 
     return(
--- frontend/src/NotFound.js
+++ frontend/src/NotFound.js
@@ -1,11 +1,17 @@
-import { Link } from "react-router-dom";
+import Button from '@mui/material/Button';
+import ErrorIcon from '@mui/icons-material/Error';
 
 const NotFound = () => {
     return (
         <div className="not-found">
-            <h2>Sorry</h2>
-            <p>That page cannot be found</p>
-            <Link to="/">Bask to the home page</Link>
+            <ErrorIcon sx={{ color: '#0066be' }} fontSize="large" />
+            <h2>Error 404 : Page not found</h2>
+            <Button
+                href="/"
+                sx={{ my: 2, color: 'white', display: 'block' }}
+            >
+                Back to the home page
+            </Button>
         </div>
     );
 }
