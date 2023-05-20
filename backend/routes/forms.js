ff7bc9123c0dba38ad5c1214bebc27eb53d1c88e
hugo-baptista <pg50416@alunos.uminho.pt>
2023-05-20 18:55:12 +0100

    Update /list (post to get)

--- backend/routes/forms.js
+++ backend/routes/forms.js
@@ -1,16 +1,37 @@
 var express = require('express');
 var router = express.Router();
-
 var UserModel = require('../model/User')
 var FormModel = require('../model/Form');
 var FormController = require('../controller/Form');
-
 const { parseNestedJSON } = require('../static/functions')
+
+router.get('/list', async (req, res) => {
+  const {current_user} = req.body;
+  const {username, password} = current_user;
+
+  UserModel.findOne({username, password})
+  .then(async user => {
+    if (user.permissions.view_forms) {
+      FormModel.find()
+      .then(async forms => {
+        res.status(200).json({success: true, forms: forms});
+      })
+      .catch(err => {
+        res.status(200).json({success: false, info: err});
+      })
+    } else {
+      res.status(200).json({success: false, info: "Não tem permissões!"});
+    }
+  })
+  .catch(err => {
+    res.json(err);
+  })
+});
 
 router.post('/submit', async (req, res) => {
   const {current_user} = req.body;
-  
   const {username, password} = current_user;
+  
   UserModel.findOne({username, password})
   .then(async user => {
     if (user.permissions.create_forms_fhir) {
@@ -25,29 +46,6 @@ router.post('/submit', async (req, res) => {
         res.status(200).json({success: false, info: "Erro ao adicionar FormComposition!"});
         console.log(newFormResponse.response);
       };
-    } else {
-      res.status(200).json({success: false, info: "Não tem permissões!"});
-    }
-  })
-  .catch(err => {
-    res.json(err);
-  })
-});
-
-router.post('/list', async (req, res) => {
-  const {current_user} = req.body;
-  
-  const {username, password} = current_user;
-  UserModel.findOne({username, password})
-  .then(async user => {
-    if (user.permissions.view_forms) {
-      FormModel.find()
-      .then(async forms => {
-        res.status(200).json({success: true, forms: forms});
-      })
-      .catch(err => {
-        res.status(200).json({success: false, info: err});
-      })
     } else {
       res.status(200).json({success: false, info: "Não tem permissões!"});
     }
